3f0ad27353e32b32f4038e0c837564595906f690
ElhamFadel <elham2000Fadel@gmail.com>
2021-09-15 14:19:43 +0300

    6-fetch-data relates #6

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -3,6 +3,7 @@ import "./style.css"
 
 export default class BookCard extends Component {
     render() {
+
         return (
             <div>
                 {/* for one book (title , price , name og book , outher ,img ) */}

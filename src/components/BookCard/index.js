2b5ca21642dd71ad25f8503dbea58eb81ce44c8b
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-09-16 12:28:05 +0300

    read me edit

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -3,45 +3,46 @@ import BookCard from "./BookCard";
 import fetchbook from "../../api/fetchbook";
 import "./style.css";
 
-
-
 export default class BookCards extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
     };
   }
   componentDidMount() {
-    fetchbook((data)=>{
-       this.setState({books: data});
-    })
+    fetchbook((data) => {
+      this.setState({ books: data });
+    });
   }
 
   render() {
-    const {value} =this.props;
+    const { value } = this.props;
     console.log(value);
-    const {books} = this.state;
-    const filterBooks = books.filter((book) => {return book.title.toLowerCase().includes(value.toLowerCase()) || book.author.toLowerCase().includes(value.toLowerCase()) });
-    console.log(books[0])
+    const { books } = this.state;
+    const filterBooks = books.filter((book) => {
+      return (
+        book.title.toLowerCase().includes(value.toLowerCase()) ||
+        book.author.toLowerCase().includes(value.toLowerCase())
+      );
+    });
+    console.log(books[0]);
     return (
       <div className="books-wrapper">
-
-        
-           {
-             books.length?
-             value === "" ? 
-             books.map((book)=><BookCard id={book.rank} details={book}/>):
-            
-            filterBooks.length ?
-             filterBooks.map((book)=><BookCard id={book.rank} details={book}/>):
-
-            <h1>Please Try Again ...  </h1>
-            :
-            <h1>Loading ...  </h1>
-            
-           }        
+        {books.length ? (
+          value === "" ? (
+            books.map((book) => <BookCard id={book.rank} details={book} />)
+          ) : filterBooks.length ? (
+            filterBooks.map((book) => (
+              <BookCard id={book.rank} details={book} />
+            ))
+          ) : (
+            <h1>Please Try Again ... </h1>
+          )
+        ) : (
+          <h1>Loading ... </h1>
+        )}
       </div>
-    ); 
+    );
   }
 }
